f005a7149c2dc53ae76414e49657e8a033b9ca33
nazarov_nv <nikolaynazarovvv216@gmail.com>
2022-05-12 12:48:04 +0300

    3-6-3 fineshed

--- leasson 3-6-3.py
+++ leasson 3-6-3.py
@@ -1,16 +1,17 @@
 import requests
 data_from_file = open ("text.txt", 'r')
 data_string=data_from_file.read()
-
+r = requests.get(data_string)
+data_string = r.text
 
 
 while True:
     try:
-        r = requests.get(data_string)
-        text_from_file = r.text
-        print(text_from_file)
+        r = requests.get("https://stepic.org/media/attachments/course67/3.6.3/"+data_string)
+        data_string = r.text
+        print(data_string)
     except:
-        print(text_from_file)
+        print(data_string)
         pass
 
 
